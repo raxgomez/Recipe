3f262358e335ebe4c3aedf8a42548d20aeef993a
Rax <rafa_gomez24@hotmail.com>
2018-03-26 17:00:48 -0600

    Created file

---
+++ eJuice_Recipes/eJuice_Recipes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  eJuice_Recipes
+//
+//  Created by Rax Gómez on 26/03/18.
+//  Copyright © 2018 Rax Gómez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
